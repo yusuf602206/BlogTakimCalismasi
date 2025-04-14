d24a83a366c840cc140516a65eb1d1d6f1b4b621
yusuf602206 <ys9123049@gmail.com>
2025-04-14 22:27:51 +0300

    BlogTakimCalisması
    BlogTakimCalisması

---
+++ OrtakBlog/Views/Home/Service.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OrtakBlog.Views.Home
+{
+    public class ServiceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
